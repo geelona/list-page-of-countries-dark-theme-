c4324b0650d6ea88b1a4dc4fdf0abe461bdc2815
geelona <mr.geelona@gmail.com>
2023-10-25 20:59:18 +0200

    done with html && css for theme switcher and country search container/bar

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,14 @@
 import '../scss/style.scss'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.body
+  const themeSwitcher = document.getElementById('theme-switcher-input') as HTMLInputElement
+
+  themeSwitcher.addEventListener('change', () => {
+    if (body.classList.contains('darkmode')) {
+      body.classList.remove('darkmode')
+    } else {
+      body.classList.add('darkmode')
+    }
+  })
+})
