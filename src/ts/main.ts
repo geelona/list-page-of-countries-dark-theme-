1a0849fba4ec0e90d376e6f2de93753e8ba79a8a
geelona <mr.geelona@gmail.com>
2023-10-27 17:06:29 +0200

    finished with API fetching and country rendering, done with a project

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,9 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const body = document.body
   const themeSwitcher = document.getElementById('theme-switcher-input') as HTMLInputElement
 
+  const countrySearchInput = document.getElementById('country-search-input') as HTMLInputElement
+  const countrySearchSubmit = document.getElementById('country-search-submit') as HTMLButtonElement
+
   const selectRegionButton = document.getElementById('select-region-button') as HTMLButtonElement
   const caret = document.getElementById('caret') as HTMLElement
   const selectRegionList = document.getElementById('select-region-list') as HTMLUListElement
+
+  const countryCardContainer = document.querySelector('.country-card-container') as HTMLDivElement
+
+  countrySearchSubmit.addEventListener('click', () => {
+    const nameFilter = countrySearchInput.value
+    const selected = selectRegionButton.querySelector('.selected') as HTMLSpanElement
+    
+    renderCountries(selected.textContent, nameFilter)
+  })
 
   selectRegionButton.addEventListener('click', () => {
     selectRegionList.classList.toggle('select-menu-active')
@@ -23,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
           el.classList.remove('select-menu-el-active')
         })
         target.classList.add('select-menu-el-active')
+        renderCountries(target.textContent, countrySearchInput.value)
       }
       selectRegionList.classList.remove('select-menu-active')
       caret.classList.remove('caret-active')
@@ -36,4 +49,53 @@ document.addEventListener('DOMContentLoaded', () => {
       body.classList.add('darkmode')
     }
   })
+
+  function countryToHTML(country: any) {
+    return `
+    <div class="card">
+      <img class="card-img-top" src="${country.flags.png}" alt="${country.name.common} flag image">
+      <div class="card-body">
+        <h5 class="card-title">${country.name.common}</h5>
+        <p class="card-text">Population: ${country.population}</p>
+        <p class="card-text">Region: ${country.region}</p>
+        <p class="card-text">Capital: ${country.capital}</p>
+      </div>
+    </div>
+  `
+  }
+
+  async function renderCountries(regionFilter: string | null = '', nameFilter: string | null = '') {
+    const response = await fetch('https://restcountries.com/v3.1/all')
+    const data = await response.json()
+
+    if (regionFilter === 'All' || regionFilter === 'Region') {
+      regionFilter = ''
+    }
+
+    let countries: string[] = []
+
+    if (regionFilter && nameFilter) {
+      countries = data.filter((country: any) => {
+        return country.region === regionFilter && country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
+      }).map((country: any) => countryToHTML(country))
+    }
+    else if (regionFilter) {
+      countries = data.filter((country: any) => {
+        return country.region === regionFilter
+      }).map((country: any) => countryToHTML(country))
+    }
+    else if (nameFilter) {
+      countries = data.filter((country: any) => {
+        return country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
+      }).map((country: any) => countryToHTML(country))
+    }
+    else {
+      countries = data.map((country: any) => countryToHTML(country))
+    }
+
+    countryCardContainer.innerHTML = ''
+    countryCardContainer.insertAdjacentHTML('afterbegin', countries.join(''))
+  }
+  renderCountries()
+
 })
